fc923f95f5d83493384a29a782a78b963600aa51
Jesse Okoro <okorojesse@gmail.com>
2024-04-03 14:11:08 +0100

    longest substring

--- kotlin-solutions/src/main/kotlin/sliding_window/LongestSubstring.kt
+++ kotlin-solutions/src/main/kotlin/sliding_window/LongestSubstring.kt
@@ -1,2 +1,21 @@
 package sliding_window
 
+import java.lang.Integer.max
+
+fun lengthOfLongestSubstring(s: String): Int {
+    val chars = BooleanArray(255) { false }
+    var longest = 0
+    var left = 0
+    var right = 0
+
+    while (right < s.length){
+        while (right < s.length && !chars[s[right].code]){
+            chars[s[right].code] = true
+            right++
+        }
+        longest = max(right - left, longest)
+        chars[s[left].code] = false
+        left++
+    }
+    return longest
+}
